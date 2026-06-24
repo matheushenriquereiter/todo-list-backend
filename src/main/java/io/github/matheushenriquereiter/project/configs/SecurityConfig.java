5236b1f98afb11861053945b833ca6513a7a5daa
matheushenriquereiter <mathenriquereiter@gmail.com>
2026-06-23 21:43:24 -0300

    Added route to get user tasks

--- src/main/java/io/github/matheushenriquereiter/project/configs/SecurityConfig.java
+++ src/main/java/io/github/matheushenriquereiter/project/configs/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
-                        .requestMatchers( "/user/register", "/user/login", "/css/**", "/js/**").permitAll()
+                        .requestMatchers( "/users/register", "/users/login", "/css/**", "/js/**").permitAll()
                         .anyRequest().authenticated())
                 .logout(logout -> logout
                         .logoutUrl("/logout")
--- src/main/java/io/github/matheushenriquereiter/project/controllers/TaskController.java
+++ src/main/java/io/github/matheushenriquereiter/project/controllers/TaskController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/task")
+@RequestMapping("/tasks")
 @CrossOrigin(origins = "http://localhost:5173/")
 public class TaskController {
     public final TaskService taskService;
