3df70dbc66f3c400e63b9d2154d4777392d35df7
matheushenriquereiter <mathenriquereiter@gmail.com>
2026-06-23 21:44:17 -0300

    Reapply "Added route to get user tasks"
    This reverts commit 3b96fa366453ee0af9ed921defa20aec4846411a.

--- src/main/java/io/github/matheushenriquereiter/project/controllers/UserController.java
+++ src/main/java/io/github/matheushenriquereiter/project/controllers/UserController.java
@@ -3,13 +3,18 @@ package io.github.matheushenriquereiter.project.controllers;
 import io.github.matheushenriquereiter.project.dtos.JwtTokenDTO;
 import io.github.matheushenriquereiter.project.dtos.UserRegisterDTO;
 import io.github.matheushenriquereiter.project.dtos.UserLoginDTO;
+import io.github.matheushenriquereiter.project.models.Task;
+import io.github.matheushenriquereiter.project.models.User;
 import io.github.matheushenriquereiter.project.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Set;
+
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 @CrossOrigin(origins = "http://localhost:5173/")
 public class UserController {
     private final UserService userService;
@@ -30,5 +35,12 @@ public class UserController {
         JwtTokenDTO jwtTokenDTO = userService.login(userLoginDTO);
 
         return ResponseEntity.ok(jwtTokenDTO);
+    }
+
+    @GetMapping("/{userId}/tasks")
+    public ResponseEntity<Set<Task>> getUserTasks(@PathVariable Long userId) {
+        Set<Task> userTasks = userService.getTasks(userId);
+
+        return ResponseEntity.ok(userTasks);
     }
 }
