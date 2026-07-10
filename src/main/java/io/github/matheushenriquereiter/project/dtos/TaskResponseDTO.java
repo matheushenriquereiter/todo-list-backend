519f3f376fa098a246e723585425bb57155992f0
matheushenriquereiter <mathenriquereiter@gmail.com>
2026-07-09 21:40:06 -0300

    Fix validation messages

--- src/main/java/io/github/matheushenriquereiter/project/dtos/TaskResponseDTO.java
+++ src/main/java/io/github/matheushenriquereiter/project/dtos/TaskResponseDTO.java
@@ -1,6 +1,7 @@
 package io.github.matheushenriquereiter.project.dtos;
 
 import io.github.matheushenriquereiter.project.enums.TaskStatus;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,18 +14,20 @@ import java.time.Instant;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TaskResponseDTO {
-    @NotNull(message = "Task ID cannot be empty")
+    @NotNull(message = "Task ID cannot be null")
     private Long id;
 
-    @NotNull(message = "Task title cannot be empty")
+    @NotNull(message = "Task title cannot be null")
     @Size(min = 3, max = 50, message = "Task title must be between 3 and 50 characters long")
     private String title;
 
-    @NotNull(message = "Task description cannot be empty")
+    @NotNull(message = "Task description cannot be null")
     @Size(min = 3, max = 50, message = "Task description must be between 3 and 50 characters long")
     private String description;
 
+    @NotNull(message = "Task status cannot be null")
     private TaskStatus status;
 
+    @NotNull(message = "Task createdAt cannot be null")
     private Instant createdAt;
 }
